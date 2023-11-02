e015b2975d59378540395cd53e2bff5a1c12a262
Mahnoosh <mahnooshn0@gmail.com>
2023-11-02 10:07:27 +0330

    Add new Controller and Views

---
+++ Online shop web app/Online shop web app/Controllers/CheckoutController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_shop_web_app.Controllers;
+
+public class CheckoutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Online shop web app/Online shop web app/Controllers/ContactUsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_shop_web_app.Controllers;
+
+public class ContactUsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
