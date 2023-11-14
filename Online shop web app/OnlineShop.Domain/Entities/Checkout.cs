cf86376e204cb6523add39ffb8dbaf1486c430f3
Mahnoosh <mahnooshn0@gmail.com>
2023-11-14 12:03:56 +0330

    some changes in layout and checkout model

--- Online shop web app/OnlineShop.Domain/Entities/Checkout.cs
+++ Online shop web app/OnlineShop.Domain/Entities/Checkout.cs
@@ -5,7 +5,7 @@ public class Checkout
 {
     public int Id { get; set; }
     public int UserId { get; set; }
-    public int ProductId { get; set; }
+    public List<int> ProductId { get; set; }
     public int ProductQuantity { get; set; }
     public int PaymentId { get; set; }
 
