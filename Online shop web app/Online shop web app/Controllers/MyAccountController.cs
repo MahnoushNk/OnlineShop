c9576bcd202f8a713a5aefdcd55767d018a24ff8
Mahnoosh <mahnooshn0@gmail.com>
2023-11-23 14:11:53 +0330

    adding view for MyAccount

---
+++ Online shop web app/Online shop web app/Controllers/MyAccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online_shop_web_app.Controllers
+{
+    public class MyAccountController : Controller
+    {
+        #region MyAccount
+
+        [HttpGet]
+        public ActionResult MyAccount()
+        {
+            return View();
+        }
+
+        #endregion
+    }
+}
